8fe005c3ab375b11ceb21f9f5b63d268ab6d954f
Sara Daiane <sara.1907@hotmail.com>
2020-07-25 16:22:47 -0300

    [UPD] - Armazenando o estado temporário da interface do usuário em um dicionário reativo

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,12 +1,23 @@
 import { Template } from 'meteor/templating';
 import { Tasks } from '../api/tasks.js';
+import { ReactiveDict } from 'meteor/reactive-dict';
+
 
 import './task.js';
 import './body.html';
  
+Template.body.onCreated(function bodyOnCreated() {
+  this.state = new ReactiveDict();
+});
+
 Template.body.helpers({
   tasks() {
-    // Show newest tasks at the top
+    const instance = Template.instance();
+        if (instance.state.get('hideCompleted')) {
+          // If hide completed is checked, filter tasks
+          return Tasks.find({ checked: { $ne: true } }, { sort: { createdAt: -1 } });
+        }
+        // Otherwise, return all of the tasks
     return Tasks.find({}, { sort: { createdAt: -1 } });
   },
 });
@@ -28,5 +39,8 @@ Template.body.events({
 
     // Clear form
     target.text.value = '';
+  },
+  'change .hide-completed input'(event, instance) {
+    instance.state.set('hideCompleted', event.target.checked);
   },
 });
