0adf428d5af47f68d61bd805ef6c1d85a9a2e9d5
Sara Daiane <sara.1907@hotmail.com>
2020-07-25 16:05:31 -0300

    [UPD] #body - Adding tasks with a form

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -6,6 +6,27 @@ import './body.html';
  
 Template.body.helpers({
   tasks() {
-    return Tasks.find({});
+    // Show newest tasks at the top
+    return Tasks.find({}, { sort: { createdAt: -1 } });
+  },
+});
+
+Template.body.events({
+  'submit .new-task'(event) {
+    // Prevent default browser form submit
+    event.preventDefault();
+
+    // Get value from form element
+    const target = event.target;
+    const text = target.text.value;
+
+    // Insert a task into the collection
+    Tasks.insert({
+      text,
+      createdAt: new Date(), // current time
+    });
+
+    // Clear form
+    target.text.value = '';
   },
 });
